79600002099927d40f406dc06c1023098a6a019f
Ty <ty.cumby@gmail.com>
2021-11-13 14:07:39 -0700

    Create hook 'stub'

--- pre-commit-jenkinsfile/lint_jenkinsfile.py
+++ pre-commit-jenkinsfile/lint_jenkinsfile.py
@@ -6,3 +6,11 @@ from typing import Optional, Sequence
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("filenames", nargs="*", type=Path)
+    args = parser.parse_args(argv)
+    return_value: int = 0
+
+    return return_value
+
+
+if __name__ == "__main__":
+    SystemExit(main())
