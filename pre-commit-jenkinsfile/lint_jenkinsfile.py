9e72eb0c8386252c7ab1bbb7ce021e583f90eefd
Ty <ty.cumby@gmail.com>
2021-11-15 11:43:50 -0700

    further implementation of lint_via_http

--- pre-commit-jenkinsfile/lint_jenkinsfile.py
+++ pre-commit-jenkinsfile/lint_jenkinsfile.py
@@ -1,9 +1,16 @@
 import argparse
+from enum import IntEnum
 from pathlib import Path
-from typing import Optional, Sequence
+from typing import List, Optional, Sequence, Tuple
 
 import urllib3
 from urllib3 import HTTPResponse
+
+
+class ErrorCodes(IntEnum):
+    OK = (0,)
+    NO_JENKINS_CRUMB = 1
+    HAD_ERRORS = 2
 
 
 def get_jenkins_crumb(jenkins_url: str) -> Optional[str]:
@@ -11,21 +18,40 @@ def get_jenkins_crumb(jenkins_url: str) -> Optional[str]:
     http = urllib3.PoolManager()
     request_url: str = f'{jenkins_url}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)'
     response: HTTPResponse = http.request("GET", request_url)
-    if response:
-        data: bytes = response.data
-        crumb = data.decode("UTF-8")
+    if response.status == 200:
+        crumb = response.decode_content()
     else:
+        print(
+            f"Requesting the crumb field from the Jenkins server failed with status {response.status}."
+        )
         crumb = None
 
     return crumb
 
 
-def lint_via_http(jenkins_url: str, filename: Path):
+def lint_via_http(jenkins_url: str, filename: Path) -> Tuple[ErrorCodes, str]:
+    return_code: ErrorCodes.OK
+    error_message: str = ""
+
     header = get_jenkins_crumb(jenkins_url)
     if header:
         http = urllib3.PoolManager()
         request_url: str = f"{jenkins_url}/pipeline-model-converter/validate"
-        response: HTTPResponse = http.request("POST", request_url, headers=header)
+        jenkinsfile_text: str = filename.read_text()
+
+        response: HTTPResponse = http.request_encode_body(
+            "POST",
+            request_url,
+            fields={"jenkinsfile": jenkinsfile_text},
+            headers=header,
+        )
+    else:
+        return_code = ErrorCodes.NO_JENKINS_CRUMB
+        error_message = (
+            "No crumb was returned by the Jenkins server and so we cannot lint."
+        )
+
+    return return_code, error_message
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -40,6 +66,14 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     )
     args = parser.parse_args(argv)
     return_value: int = 0
+
+    filenames: List[Path] = []
+    if args.filenames:
+        filenames = args.filenames
+
+    jenkins_url: str = ""
+    if args.jenkins_url:
+        jenkins_url = args.jenkins_url
 
     return return_value
 
